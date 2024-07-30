d5d9b591983055bc531ae5fb3a4ad943345138c9
Agustin Viera <facundoviera98@gmail.com>
2024-07-30 20:01:15 -0300

    style: Aplicar patron de diseño al proyecto
    - Aplicar patron de diseño Riverpod
    - Aplicar patron feature first
    
    Realizado-por: Agustin Viera

---
+++ lib/app/pombo_wallet.dart
@@ -0,0 +1,12 @@
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pombo wallet',
+    );
+  }
+}
