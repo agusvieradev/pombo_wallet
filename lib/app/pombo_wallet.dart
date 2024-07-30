a8db874212c0ae209acb31f9e9fe97c3af902acd
Agustin Viera <facundoviera98@gmail.com>
2024-07-30 20:18:46 -0300

    refact: Borrar comentarios innecesarios

--- lib/app/pombo_wallet.dart
+++ lib/app/pombo_wallet.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 
 class PomboWallet extends StatelessWidget {
   const PomboWallet({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
